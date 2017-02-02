c97ce659b99a372a50cebfa9967a2e5fc4637adc
Gustavo Nishihara <gustavonishihara@hotmail.com>
2017-02-02 15:41:56 -0200

    Added calibration method

--- MIDI.cpp
+++ MIDI.cpp
@@ -17,23 +17,110 @@ void MIDI::receive(byte command, byte note, byte velocity)
 
 }
 
-int MIDI::decode(byte note, byte velocity) // parameter for stick() and stone()
-{
-	switch(note)
+int MIDI::decode(byte command, byte note, byte velocity, int *parameter, int *angle) // parameter for stick() and stone()
+{	
+	if (command == NOTEON_CH0)
 	{
-		case NOTE_DOM : // run stick()
-			return 1;
-		break;
+		switch(note)
+		{
+			case NOTE_DOM : // run stick()
+				*parameter = NOTE_DOM;
+			break;
 
-		case NOTE_DIM : // run stick and stone() / stone full strength
-			return 2;
-		break;
+			case NOTE_DIM : // run stick and stone() / stone full strength
+				*parameter = NOTE_DIM;
+			break;
 
-		case NOTE_TIM : // run stick and stone() / stone half strength
-			return 3;
-		break;
-
-		default :
-			return 0;
+			case NOTE_TIM : // run stick and stone() / stone half strength
+				*parameter = NOTE_TIM;
+			break;
+		}
+		return 0;
 	}
+
+	if (command == CONTROL_CHANGE)
+	{
+		switch(note)
+		{
+
+			case CONTROL_1 :
+				*parameter = NOTE_DOM;
+				break;
+
+			case CONTROL_2 :
+				*parameter = NOTE_DIM;
+				break;
+
+			case CONTROL_3 :
+				*parameter = NOTE_TIM;
+				break;
+
+			default :
+
+		}
+		return 1;
+	}
+	if (command == CONTROL_CHANGE && note == CONTROL_0)
+	{
+		*angle = velocity;
+		return 2;
+	}
+
+
+
+
+
+
+
+
+	int state = 0;
+	if (command == NOTEON_CH0)
+	{
+		state = 1; // Play note
+	}
+	else if (command == CONTROL_CHANGE)
+	{
+		state = 2; // calibration
+	}
+	if (state == 1)
+	{
+		switch(note)
+		{
+			case NOTE_DOM : // run stick()
+				return 1;
+			break;
+
+			case NOTE_DIM : // run stick and stone() / stone full strength
+				return 2;
+			break;
+
+			case NOTE_TIM : // run stick and stone() / stone half strength
+				return 3;
+			break;
+
+			default :
+				return 0;
+		}
+	}
+	if (state == 2)
+	{
+		if (note == CONTROL_0)
+		{
+			state = 3; // 
+		}
+		else if(note == CONTROL_1)
+		{
+			state = 4;
+		}
+	}
+	if (state == 3)
+	{
+		*parameter = velocity;
+	}
+	if (state == 4)
+	{
+		
+	}
+	
 }
+
--- MIDI.h
+++ MIDI.h
@@ -7,6 +7,11 @@
 #define NOTE_DOM 60 // dom note of berimbau assigned to MIDI C4
 #define NOTE_DIM 61 // dim note of berimbau assigned to MIDI C#4
 #define NOTE_TIM 62 // tim note of berimbau assigned to MIDI D4
+#define CONTROL_CHANGE 176 // Control signal
+#define CONTROL_0 1 // Controller 0
+#define CONTROL_1 2 // Controller 1
+#define CONTROL_2 3 // Controller 2
+#define CONTROL_3 4 // Controller 3
 
 class MIDI{
 public:
